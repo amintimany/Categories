422665206cbca879e1490519509e6836901807fa
Amin Timany <amintimany@gmail.com>
2015-07-16 15:17:56 +0200

    Cleaned up and documented Archetypal/PreOrder_Cat.v. It was not part of the _CoqProject file and was thus not being compiled!

--- Archetypal/PreOrder_Cat.v
+++ Archetypal/PreOrder_Cat.v
@@ -3,68 +3,44 @@ Require Import Category.Main.
 (** Every pre-order relation is a category. *)
 Record PreOrder : Type :=
 {
-  PreOrder_car : Type;
+  PreOrder_car :> Type;
   
-  PreOrder_rel : PreOrder_car → PreOrder_car → Prop;
+  PreOrder_rel : PreOrder_car → PreOrder_car → Prop where "a < b" := (PreOrder_rel a b);
 
-  PreOrder_refl : ∀ a, PreOrder_rel a a;
+  PreOrder_refl : ∀ a, a < a;
 
-  PreOrder_trans : ∀ a b c, PreOrder_rel a b → PreOrder_rel b c → PreOrder_rel a c
+  PreOrder_trans : ∀ a b c, a < b → b < c → a < c
 }.
+
+Arguments PreOrder_rel {_} _ _.
+Arguments PreOrder_refl {_} _.
+Arguments PreOrder_trans {_ _ _ _} _ _.
+
+Notation "a < b" := (PreOrder_rel a b) : preorder_scope.
 
 Section PreOrder_Cat.
   Context (P : PreOrder).
 
-  Inductive PreOrder_Hom (a b : @PreOrder_car P) : Type :=
-  | PreOrder_Arrow : PreOrder_rel a b → PreOrder_Hom a b
+  Local Open Scope preorder_scope.
+
+  (* if goal is equality of two preorder relation proofs, we get the to the hypotheses
+so that they can be processed by proof irrelevance tactic (PIR). *)
+  Local Hint Extern 1 =>
+  match goal with
+    [|- ?A = ?B :> (_ < _) ] => set A; set B
+  end.
+
+  Local Hint Extern 1 => PIR. (* automatically apply proof irrelevance *)
+  
+  Local Hint Resolve PreOrder_refl PreOrder_trans.
+
+  Local Obligation Tactic := basic_simpl; eauto.
+
+  Program Definition PreOrder_Cat : Category :=
+    {|
+      Obj := P;
+      Hom := fun a b => a < b
+    |}
   .
 
-  Hint Constructors PreOrder_Hom.
-
-  Hint Extern 1 =>
-  match goal with
-      [H : PreOrder_Hom _ _ |- _] =>
-      destruct H; simpl in *
-  end.
-
-  Hint Extern 1 =>
-  match goal with
-      [|- PreOrder_Arrow ?A ?B ?H1 = PreOrder_Arrow ?A ?B ?H2] =>
-      destruct (proof_irrelevance _ H1 H2); reflexivity
-  end.
-
-  Hint Extern 1 =>
-  match goal with
-      [|- PreOrder_Arrow ?A ?B ?H1 = PreOrder_Arrow ?A ?B ?H2] =>
-      destruct (proof_irrelevance _ H2 H1); reflexivity
-  end.
-
-  Hint Resolve PreOrder_refl PreOrder_trans.
-
-  Local Obligation Tactic := program_simpl; eauto.
-
-  Program Instance PreOrder_Cat : Category (@PreOrder_car P) (PreOrder_Hom).
-
 End PreOrder_Cat.
-
-Hint Constructors PreOrder_Hom.
-
-Hint Extern 1 =>
-match goal with
-    [H : PreOrder_Hom _ _ _ |- _] =>
-    destruct H; simpl in *
-end.
-
-Hint Extern 1 =>
-match goal with
-    [|- PreOrder_Arrow ?P ?A ?B ?H1 = PreOrder_Arrow ?P ?A ?B ?H2] =>
-    destruct (proof_irrelevance _ H1 H2); reflexivity
-end.
-
-Hint Extern 1 =>
-match goal with
-    [|- PreOrder_Arrow ?P ?A ?B ?H1 = PreOrder_Arrow ?P ?A ?B ?H2] =>
-    destruct (proof_irrelevance _ H2 H1); reflexivity
-end.
-
-Hint Resolve PreOrder_refl PreOrder_trans.
