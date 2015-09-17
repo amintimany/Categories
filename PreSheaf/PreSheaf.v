30b299b9f150da8210540efef53b1c21df475777
Amin Timany <amintimany@gmail.com>
2015-09-17 15:52:42 +0200

    Added presheafs.

---
+++ PreSheaf/PreSheaf.v
@@ -0,0 +1,13 @@
+Require Import Essentials.Notations.
+Require Import Essentials.Types.
+Require Import Essentials.Facts_Tactics.
+Require Import Category.Main.
+Require Import Functor.Main.
+Require Import Coq_Cats.Type_Cat.Type_Cat.
+Require Import NatTrans.NatTrans NatTrans.Func_Cat.
+
+(** A presheaf on C is a functor Cᵒᵖ –≻ Type_Cat. *)
+Definition PreSheaf (C : Category) := Functor (C^op) Type_Cat.
+
+(** The category of presheafs. *)
+Definition PShCat (C : Category) := Func_Cat (C^op) Type_Cat.
