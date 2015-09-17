96fa8371262d3b9f6f29efbdcaa774fb82530f75
Amin Timany <amintimany@gmail.com>
2015-09-17 19:00:01 +0200

    Added terminal object, products and exponentials of presheafs.

---
+++ PreSheaf/Product.v
@@ -0,0 +1,69 @@
+Require Import Essentials.Notations.
+Require Import Essentials.Types.
+Require Import Essentials.Facts_Tactics.
+Require Import Category.Main.
+Require Import Functor.Main.
+Require Import Coq_Cats.Type_Cat.Type_Cat Coq_Cats.Type_Cat.CCC.
+Require Import NatTrans.NatTrans NatTrans.Func_Cat.
+Require Import Basic_Cons.Product.
+Require Import PreSheaf.PreSheaf.
+
+Section Product.
+  Context (C : Category) (F G : PreSheaf C).
+
+  (** The pointwise product presheaf. *)
+  Program Definition pointwise_product_psh : Functor (C^op) Type_Cat :=
+    {|
+      FO := fun x => ((F _o x) * (G _o x))%object%type;
+      FA := fun _ _ f u => (F _a f (fst u), G _a f (snd u))%morphism%object
+    |}.
+
+  Next Obligation.
+  Proof.
+    FunExt.
+    rewrite (F_id F); rewrite (F_id G); trivial.
+  Qed.
+
+  Next Obligation.
+  Proof.
+    FunExt.
+    cbn_rewrite (F_compose F f g).
+    cbn_rewrite (F_compose G f g).
+    trivial.
+  Qed.
+
+  Local Hint Extern 1 =>
+  repeat
+    match goal with
+      [f : (?p –≻ _)%nattrans, h : (_ –≻ _)%morphism, c : _, x : (?p _o)%object _ |- _] =>
+      cbn_rewrite (equal_f (Trans_com f h) x)
+    end.
+
+  (** The pointwise product presheaf is the product of presheafs. *)
+  Program Definition PSh_Product : @Product (PShCat C) F G:=
+    {|
+      product := pointwise_product_psh;
+      Pi_1 := {| Trans := fun _ => fst |};
+      Pi_2 := {| Trans := fun _ => snd |};
+      Prod_morph_ex := fun p' f g => {|Trans := fun x u => (Trans f x u, Trans g x u) |}
+    |}.
+
+  Local Obligation Tactic := idtac.
+  
+  Next Obligation.
+  Proof.
+    intros p' r1 r2 f g H1 H2 H3 H4; cbn in *.
+    rewrite <- H3 in H1; rewrite <- H4 in H2; clear H3 H4.
+    apply NatTrans_eq_simplify.
+    extensionality v; extensionality z.
+    assert (W1 := f_equal (fun w : (p' –≻ F)%nattrans => Trans w v z) H1).
+    assert (W2 := f_equal (fun w : (p' –≻ G)%nattrans => Trans w v z) H2).
+    cbn in W1, W2.
+    match goal with
+      [|- ?A = ?B] => destruct A; destruct B; cbn in *; auto
+    end.
+  Qed.    
+
+End Product.
+
+Instance PSh_Has_Products (C : Category) : Has_Products (PShCat C) := PSh_Product C.
---
+++ PreSheaf/Terminal.v
@@ -0,0 +1,30 @@
+Require Import Essentials.Notations.
+Require Import Essentials.Types.
+Require Import Essentials.Facts_Tactics.
+Require Import Category.Main.
+Require Import Functor.Main.
+Require Import Coq_Cats.Type_Cat.Type_Cat Coq_Cats.Type_Cat.CCC.
+Require Import NatTrans.NatTrans NatTrans.Func_Cat.
+Require Import Basic_Cons.Terminal.
+Require Import PreSheaf.PreSheaf.
+
+Section Terminal.
+  Context (C : Category).
+
+  (** The terminal element of the category of presheafs. *)
+  Program Definition PSh_Term_Func : Functor (C^op) Type_Cat :=
+    {|
+      FO := fun _ => UNIT
+    |}.
+
+  Local Hint Resolve NatTrans_eq_simplify.
+  Local Hint Resolve UNIT_SINGLETON.
+
+  (** The functor that maps to the unit type in coq is the terminal object of presheafs. *)
+  Program Instance PSh_Terminal : Terminal (PShCat C) :=
+    {
+      terminal := PSh_Term_Func;
+      t_morph := fun _ => {|Trans := fun _ _ => TT|}
+    }.
+
+End Terminal.
