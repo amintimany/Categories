48bcd92c75a1d689c976634fe10a22e3f8d47217
Amin Timany <amintimany@gmail.com>
2015-09-26 17:22:51 +0200

    Proved that if F≃F' then (Comma F G)≃(Comma F' G) and dually if G≃G' then (Comma F G)≃(Comma F G').

--- Ext_Cons/Comma.v
+++ Ext_Cons/Comma.v
@@ -5,7 +5,8 @@ Require Import Category.Category Category.Morph Category.Opposite.
 Require Import Ext_Cons.Arrow.
 Require Import Functor.Functor Functor.Functor_Ops Const_Func.
 Require Import Archetypal.Discr.Discr.
-Require Import Cat.Cat.
+Require Import Cat.Cat Cat.Cat_Iso.
+Require Import NatTrans.NatTrans NatTrans.NatIso.
 
 Local Open Scope morphism_scope.
 
@@ -196,7 +197,177 @@ Section Comma_Opposite_Iso.
   .
 
 End Comma_Opposite_Iso.
+
+(** In this section we show that whenever F and F' are two naturally
+isomorphic functors, then (Comma F G) is isomorphic to (Comma F' G).
+ *)
+Section Comma_Left_Func_Iso.
+  Context {B C D : Category}.
+
+  Local Hint Extern 1 => progress cbn.
+
+  Local Hint Extern 1 => apply Comma_Hom_eq_simplify.
+
+  Section Comma_Left_Func_Iso_FC.
+    Context
+      {F F' : (B –≻ C)%functor}
+      (I : (F ≃ F')%natiso)
+      (G : (D –≻ C)%functor)
+    .
+
+    (** Given a natural isomorphism we build a functor. *)
+    Program Definition Comma_Left_Func_Iso_FC :
+      Functor (Comma F G) (Comma F' G)
+      :=
+        {|
+          FO :=
+            fun x =>
+              {|
+                CMO_src := CMO_src x;
+                CMO_trg := CMO_trg x;
+                CMO_hom := (CMO_hom x) ∘ (Trans (inverse_morphism I) (CMO_src x))
+              |};
+          FA :=
+            fun c c' h =>
+              {|
+                CMH_left := CMH_left h;
+                CMH_right := CMH_right h;
+              CMH_com := _
+              |}
+        |}.
+    
+    Next Obligation.
+    Proof.
+      rewrite assoc_sym.
+      rewrite (CMH_com h).
+      rewrite assoc.
+      rewrite <- (Trans_com (inverse_morphism I) (CMH_left h)).
+      auto.    
+    Qed.
+    
+  End Comma_Left_Func_Iso_FC.
+
+  Section Comma_Left_Func_Iso_FC_Iso.
+    Context
+      {F F' : (B –≻ C)%functor}
+      (I : (F ≃ F')%natiso)
+      (G : (D –≻ C)%functor)
+    .
+
+    (** Functors produced from a natural isomorphism and its inverse
+are inverses.
+*)
+    Lemma Comma_Left_Func_Iso_FC_Iso :
+      ((Comma_Left_Func_Iso_FC I G)
+         ∘
+         (Comma_Left_Func_Iso_FC (Inverse_Isomorphism I) G))%functor
+      =
+      Functor_id _
+    .
+    Proof.
+      Func_eq_simpl.
+      {
+        extensionality x.
+        extensionality y.
+        extensionality f.
+        match type of H with
+          ?A = ?B =>
+          set (H' :=
+                 fun x =>
+                   match H in _ = y return
+                         A x = y x
+                   with
+                     eq_refl => eq_refl
+                   end
+              )
+        end.
+        transitivity (
+            match H' x in _ = u return
+                  u –≻ _
+            with
+              eq_refl =>
+              match H' y in _ = v return
+                    _ –≻ v
+              with
+                eq_refl =>
+                ((Comma_Left_Func_Iso_FC I G ∘ Comma_Left_Func_Iso_FC (I ⁻¹) G) _a f)
+              end
+            end
+          ).
+        {
+          destruct H; trivial.
+        }
+        {
+          apply Comma_Hom_eq_simplify.
+          {
+            apply JMeq_eq.
+            destruct H; trivial.
+          }
+          {
+            apply JMeq_eq.
+            destruct H; trivial.
+          }
+        }
+      }
+      {
+        extensionality x.
+        destruct x as [s t h]; cbn.
+        rewrite assoc.
+        change ((Trans (iso_morphism I) s) ∘ Trans (I ⁻¹)%morphism s) with
+        (Trans ((iso_morphism I) ∘ (I ⁻¹)%morphism) s).
+        cbn_rewrite (right_inverse I).
+        cbn; auto.
+      }
+    Qed.
+    
+  End Comma_Left_Func_Iso_FC_Iso.
+
   
+  Context
+    {F F' : (B –≻ C)%functor}
+    (I : (F ≃ F')%natiso)
+    (G : (D –≻ C)%functor)
+  .
+
+  Local Hint Extern 1 => apply Comma_Left_Func_Iso_FC_Iso.
+  
+  Program Definition Comma_Left_Func_Iso :
+    ((Comma F G) ≃≃ (Comma F' G) ::> Cat)%isomorphism
+    :=
+      {|
+        iso_morphism := Comma_Left_Func_Iso_FC I G;
+        inverse_morphism := Comma_Left_Func_Iso_FC (Inverse_Isomorphism I) G
+      |}
+  . 
+    
+End Comma_Left_Func_Iso.
+
+(** In this section we show that whenever G and G' are two naturally
+isomorphic functors, then (Comma F G) is isomorphic to (Comma F G').
+This follows from Comma_Left_Func_Iso and Comma_Opposite_Iso proven
+above.
+ *)
+Section Comma_Right_Func_Iso.
+  Context
+    {B C D : Category}
+    (F : (B –≻ C)%functor)
+    {G G' : (D –≻ C)%functor}
+    (I : (G ≃ G')%natiso)
+  .
+
+  Definition Comma_Right_Func_Iso :
+    ((Comma F G) ≃≃ (Comma F G') ::> Cat)%isomorphism :=
+    Isomorphism_Compose
+      (
+        Isomorphism_Compose
+          (Inverse_Isomorphism (Comma_Opposite_Iso (G ^op) (F ^op)))
+          (Opposite_Cat_Iso (Comma_Left_Func_Iso (Opposite_NatIso I) (F^op)))
+      )
+      (Comma_Opposite_Iso (G' ^op) (F ^op))
+  .
+    
+End Comma_Right_Func_Iso.
+
 (**
 Slice, coslice and arrow categories are special cases of comma categories defined below:
 *)
