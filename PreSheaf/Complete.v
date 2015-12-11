7f6a6357a8fa0c3879d5e7bc699a47df71265a7a
Amin Timany <amintimany@gmail.com>
2015-12-11 13:48:10 +0100

    The name of PShCat_(Co)Complete was mistakenly writen Type_Cat_(Co)Complete.

--- PreSheaf/Complete.v
+++ PreSheaf/Complete.v
@@ -9,8 +9,8 @@ Require Import
         PreSheaf.GenSum
 .
 
-Instance Type_Cat_Complete (C: Category) : Complete (PShCat C) :=
+Instance PShCat_Complete (C: Category) : Complete (PShCat C) :=
   @GenProd_Eq_Complete (PShCat C) (PSh_GenProd C) (@PSh_Has_Equalizers C).
 
-Instance Type_Cat_CoComplete (C: Category) : CoComplete (PShCat C) :=
+Instance PShCat_CoComplete (C: Category) : CoComplete (PShCat C) :=
   @GenSum_CoEq_CoComplete (PShCat C) (PSh_GenSum C) (@PSh_Has_CoEqualizers C).
